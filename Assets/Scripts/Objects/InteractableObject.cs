e5e3871d81104093510d249e956b5df7c23c4351
Imanol Valle <mphc028@gmail.com>
2025-12-10 18:25:06 +0100

    Added everything

--- Assets/Scripts/Objects/InteractableObject.cs
+++ Assets/Scripts/Objects/InteractableObject.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
+    [SerializeField] private GameObject _light;
+    private bool _enabled = true;
+
     public void Interact()
     {
+        _enabled = !_enabled;
+        _light.SetActive(_enabled);
         Debug.Log("InteractableObject Interacted!");
     }
 }
